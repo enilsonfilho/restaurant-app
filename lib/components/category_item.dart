99961fdba8edfe528e53ca15d78289b4bff087ee
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-01-03 13:47:28 -0300

    feat: add BorderRadius

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -11,12 +11,16 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Text(category.title),
+      padding: EdgeInsets.all(15),
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
         gradient: LinearGradient(
           colors: [
             category.color.withOpacity(0.5),
             category.color,
           ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
         )
       ),
     );
