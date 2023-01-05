84d23499ef9fe4b6c9f95bcc65f771c7e27f9c87
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-01-05 17:13:59 -0300

    fic: itemCount

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
               fontSize: 20,
               fontFamily: 'RobotoCondensed',
             )),
-      ),
+        ),
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(), // Rota principal (home)
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -13,10 +13,41 @@ class MealDeatilScreen extends StatelessWidget {
           meal.title,
         ),
       ),
-      body: Center(
-        child: Text(
-         'Detalhes da Refeição',
-        ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+            ),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            width: 200,
+            height: 300,
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Text(
+                    meal.ingredients[index],
+                  ),
+                );
+              }
+            ),
+          )
+        ],
       ),
     );
   }
