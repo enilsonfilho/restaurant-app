bbb573297a41e4abf72b7ee3e87225eb0ef6c88f
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-01-05 17:21:56 -0300

    feat: BoxDecoration

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -33,15 +33,26 @@ class MealDeatilScreen extends StatelessWidget {
             ),
           ),
           Container(
-            width: 200,
-            height: 300,
+            width: 300,
+            height: 200,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
             child: ListView.builder(
               itemCount: meal.ingredients.length,
               itemBuilder: (ctx, index) {
                 return Card(
                   color: Theme.of(context).colorScheme.secondary,
-                  child: Text(
-                    meal.ingredients[index],
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10
+                    ),
+                    child: Text(meal.ingredients[index]),
                   ),
                 );
               }
