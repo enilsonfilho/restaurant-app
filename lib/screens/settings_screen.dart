7fe33af6e416be94e716f1725f600abcaf3fe9f2
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-01-06 16:25:35 -0300

    feat: add drawer in settings

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_restaurante/components/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -8,6 +9,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Configurações'),
       ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Configurações!'),
       ),
