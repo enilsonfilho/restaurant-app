b40ebf94546d9bb95577a0622501801ef9b40ec2
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-01-06 16:20:56 -0300

    feat: pushReplacementNamed

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -47,12 +47,12 @@ class MainDrawer extends StatelessWidget {
           _createItem(
             Icons.restaurant, 
             'Refeições',
-            () => Navigator.of(context).pushNamed(AppRoutes.HOME),
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.HOME),
           ),
           _createItem(
             Icons.settings, 
             'Configurações',
-            () => Navigator.of(context).pushNamed(AppRoutes.SETTINGS),
+            () => Navigator.of(context).pushReplacementNamed(AppRoutes.SETTINGS),
           ),
         ],
       ),
